f028382ec89e9fea28aea9e8a613e8d46ffd68c9
Furkan-Can-Bayrak <furkancanb957@gmail.com>
2025-08-11 00:48:53 +0300

    fix

--- app/Repositories/PostgreSql/BaseRepository.php
+++ app/Repositories/PostgreSql/BaseRepository.php
@@ -6,7 +6,6 @@ use App\Repositories\Contracts\BaseRepositoryInterface;
 use App\Repositories\Criteria\QueryParameters;
 use Fukibay\StarterPack\Traits\AppliesQueryCriteria;
 use Illuminate\Contracts\Pagination\LengthAwarePaginator;
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 
